a4617a262b9fd64b283ef39f4fa1022194af62c5
Guilherme-Ramos-Nepomuceno <guilhermeramos0411@gmail.com>
2024-07-02 17:25:29 -0400

    atualizando eslint

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,23 +1,33 @@
-"use client"
+'use client'
 
-import { AlignJustify } from "lucide-react"
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
-import React from "react"
+import { AlignJustify } from 'lucide-react'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+import React from 'react'
 
-export function NavegadorMobile () { 
-    const [open, setOpen] = React.useState(false)
+export function NavegadorMobile() {
+  const [open, setOpen] = React.useState(false)
 
-    return (
-        <Sheet open={open} onOpenChange={setOpen}>
-            <SheetTrigger className="md:hidden">
-                <AlignJustify className="text-zinc-950 mr-4" />
-            </SheetTrigger>
-            <SheetContent><div className="flex flex-col gap-y-10">
-            <a className="text-3xl font-bold text-amber-950  " href="#ultimocamp">ultimo campeonato</a>
-            <a className="text-3xl font-bold text-amber-950  " href="#jornada">jornada</a>
-            <a className="text-3xl font-bold text-amber-950 mr-1 " href="#sobremandu">sobre o Manduvi</a>
-            </div>
-            </SheetContent>
-        </Sheet>
-    )
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger className="md:hidden">
+        <AlignJustify className="text-zinc-950 mr-4" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex flex-col gap-y-10">
+          <a className="text-3xl font-bold text-amber-950  " href="#ultimocamp">
+            ultimo campeonato
+          </a>
+          <a className="text-3xl font-bold text-amber-950  " href="#jornada">
+            jornada
+          </a>
+          <a
+            className="text-3xl font-bold text-amber-950 mr-1 "
+            href="#sobremandu"
+          >
+            sobre o Manduvi
+          </a>
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
 }
